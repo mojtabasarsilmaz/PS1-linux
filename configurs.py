283b3960f438781ce582d21379c49755448c2a60
Mojtaba Sarsilmaz <mojtabasarsilmaz@gmail.com>
2019-12-16 15:59:18 +0330

    py file created

---
+++ configurs.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+
+print("### Hello this app is just for change your PS1 desired for ever ###")
+print("#################### it's just for ubuntu base ####################")
+
+import os
+try:
+    import getpass as gs
+except ImportError:
+    os.system("apt-get install python3-pip")
+    os.system("pip3 install getpass4")
+
+PS1 = str(input("Enter your ps1"))
+USER = gs.getuser()
+PWD = os.getcwd()
+if USER != 'root':
+    PWD = PWD.split('/')
+    if PWD[1] == 'home':
+        PWD_N = '/'+PWD[1]+'/'+USER+'/'
+        os.chdir(PWD_N)
+        DIRs = os.listdir()
+        for target in DIRs:
+            if target == '.bashrc':
+                with open('.bashrc','a') as conf:
+                    conf.write("PS1='{0}'\n".format(PS1))
+    else:
+        PWD = '/home'
+else:
+    print('super user please change and configur your configs manual.')
